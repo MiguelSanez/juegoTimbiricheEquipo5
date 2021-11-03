e56848c96fea3eacc444a62b0c038fa6d31879f4
Meli <78457370+meli-a@users.noreply.github.com>
2021-11-03 10:21:46 -0700

    Avance

--- src/control/Control.java
+++ src/control/Control.java
@@ -9,65 +9,80 @@ public class Control {
     }
     
     public Jugador crearJugador(String nombre, String color){
+       
         int[] colores= new int[3];
+        
         switch(color){
+            
             case "Rojo":
                 colores[0]= 255;
                 colores[1]= 87;
                 colores[2]= 87;
                 break;
+            
             case "Naranja":
                 colores[0]= 255;
                 colores[1]= 145;
                 colores[2]= 77;
                 break;
+            
             case "Amarillo":
                 colores[0]= 255;
                 colores[1]= 189;
                 colores[2]= 89;
                 break;
+            
             case "Verde lima":
                 colores[0]= 201;
                 colores[1]= 226;
                 colores[2]= 101;
                 break;
+            
             case "Verde":
                 colores[0]= 0;
                 colores[1]= 128;
                 colores[2]= 55;
                 break;
+            
             case "Turquesa":
                 colores[0]= 3;
                 colores[1]= 152;
                 colores[2]= 158;
                 break;
+            
             case "Celeste":
                 colores[0]= 56;
                 colores[1]= 182;
                 colores[2]= 255;
                 break;
+            
             case "Azul":
                 colores[0]= 67;
                 colores[1]= 92;
                 colores[2]= 207;
                 break;
+           
             case "Morado":
                 colores[0]= 126;
                 colores[1]= 79;
                 colores[2]= 219;
                 break;
+           
             case "Rosa":
                 colores[0]= 255;
                 colores[1]= 102;
                 colores[2]= 196;
                 break;
+           
             case "Rosa claro":
                 colores[0]= 255;
                 colores[1]= 203;
                 colores[2]= 235;
                 break;
         }
+       
         Jugador jugador= new Jugador(colores, nombre);
         return jugador;
     }
+    
 }
